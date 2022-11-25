226783738308d2bdf8655440f413a7b926aa1840
W0lf1n <git@petrbohac.eu>
2022-11-25 09:31:46 +0100

    pridan feedback, vylepseni ulozeni uzivatelu

--- main.py
+++ main.py
@@ -1,48 +1,80 @@
 from flask import Flask, render_template, request
-import re
-
-trida = {
-    "Jmeno": [],
-    "Je_plavec": [],
-    "Jmeno_kamarada": []
-}
+import json
 
 app = Flask(__name__, static_url_path='/static', static_folder='static', template_folder='templates')
 
 
+def in_file(string):
+    with open("static/users.json", "r") as fo:
+        data = fo.read()
+        return string in data
+
+
+with open('static/users.json', 'r') as file:
+    json_object = json.load(file)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('prvni_stranka.html'), 200
+    return render_template('prvni_stranka.html', len=len(json_object), file_users=json_object), 200
 
 
 @app.route('/druha_stranka', methods=['GET', 'POST'])
 def druha_stranka():
     return render_template('druha_stranka.html', zprava="Tajná zpráva.."), 200
 
+
 @app.route('/api/name/<name>', methods=['GET'])
 def api_check(name):
-    if name in trida["Jmeno"]:
+    if in_file(name):
         res = ["True"]
         return res
     else:
         res = ["False"]
         return res
 
+
 @app.route('/form_handler', methods=['GET', 'POST'])
 def script():
     test_name = request.form['nick']
     je_plavec = request.form['je_plavec']
+    if je_plavec:
+        je_plavec = "Plavec"
+    else:
+        je_plavec = "Neplavec"
     test_friend_name = request.form['kanoe_kamarad']
     surname = request.form['jmeno']
     lastname = request.form['prijmeni']
 
-    if test_name in trida["Jmeno"]:
-        return render_template('druha_stranka.html', zprava = "Uzivatel se stejnym jmenem jiz existuje")
+    if in_file(test_name):
+        return render_template('druha_stranka.html', zprava="Uzivatel se stejnym jmenem jiz existuje")
     else:
-        trida["Jmeno"].append(test_name)
-        trida["Je_plavec"].append(je_plavec)
-        trida["Jmeno_kamarada"].append(test_friend_name)
-        return render_template('prvni_stranka.html') + str(trida)
+        data = {
+            "Jmeno": surname,
+            "Prijmeni": lastname,
+            "Prezdivka": test_name,
+            "Plave": je_plavec,
+            "Kamarad": test_friend_name
+        }
+        data_json = json.dumps(data, indent=5)
+
+        with open("static/users.json", "a") as fo:
+            fo.write(data_json)
+
+        return render_template('druha_stranka.html'), 200
+
+
+@app.route('/feedback', methods=['GET', 'POST'])
+def feedback_page():
+    return render_template('feedback.html'), 200
+
+
+@app.route('/feedback/send', methods=['GET', 'POST'])
+def feedback_script():
+    text = request.form['feedback']
+    with open('static/feedbacks.txt', 'a') as fi:
+        fi.write(text + "\n")
+    return render_template('feedback.html', zprava="Feedback uspesne ulozen"), 200
 
 
 if __name__ == '__main__':
